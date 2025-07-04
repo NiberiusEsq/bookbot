51a817c36496d4ea10e6ec0e5cc014391639619f
NiberiusEsq <noelgora90@gmail.com>
2025-07-04 22:21:49 +0200

    now prints number of words

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        return f.read()
+
+def count_words(text):
+    words = text.split()
+    num_words = 0
+    for word in words:
+        num_words += 1
+    return f"{num_words} words found in the document"
+
+def main():
+    print(count_words(get_book_text("./books/frankenstein.txt")))
+    return
+
+main()
